bb50fac3a072d032e95359b49fd1ba87858ffd13
jubayer <jubayerh@hotmail.com>
2024-07-10 12:43:12 -0400

    update java-2

--- juby.java
+++ juby.java
@@ -3,5 +3,8 @@ public class juby {
 
     public static void main(String[] args) {
         System.out.println("this time first time edit able program");
+        if (8>0){
+            System.out.println(" im sleep");
+        }
     }
 }
